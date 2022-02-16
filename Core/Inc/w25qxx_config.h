1e39a24e2c5805a8f43cccd3dc1ce2874e01bf7c
aero-error <mngrom852@gmail.com>
2022-02-15 18:43:09 -0600

    Fixed naming discrepancies

--- Core/Inc/w25qxx_config.h
+++ Core/Inc/w25qxx_config.h
@@ -3,7 +3,7 @@
 
 #define _W25QXX_SPI                   hspi2
 #define _W25QXX_CS_GPIO               GPIOC
-#define _W25QXX_CS_PIN                Flash_CS_GPIO_Pin
+#define _W25QXX_CS_PIN                SPI3_FLASH_CS_GPIO_Pin
 #define _W25QXX_USE_FREERTOS          0
 #define _W25QXX_DEBUG                 0
 
